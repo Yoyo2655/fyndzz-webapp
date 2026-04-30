c28c530f7d7491643dea2264c547c9d7e2e28bd3
Yoni Attal <yonimilo55@gmail.com>
2026-04-30 18:11:32 +0200

    init pwa

---
+++ app/manifest.js
@@ -0,0 +1,28 @@
+export default function manifest() {
+  return {
+    name: 'Fyndzz — Find it, Park it',
+    short_name: 'Fyndzz',
+    description: 'Trouvez une place de stationnement disponible en temps réel',
+    start_url: '/',
+    display: 'standalone',
+    orientation: 'portrait',
+    background_color: '#160C6B',
+    theme_color: '#3D2CD5',
+    icons: [
+      {
+        src: '/Logo-RBG_Fyndzz.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'any maskable'
+      },
+      {
+        src: '/Logo-RBG_Fyndzz.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable'
+      }
+    ],
+    categories: ['navigation', 'travel', 'utilities'],
+    lang: 'fr',
+  }
+}
